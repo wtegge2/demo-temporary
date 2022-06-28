95b5b0ab97a48b72660388f6045488102cd38e3f
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2022-06-28 10:27:31 -0500

    adding all

---
+++ tasks.py
@@ -0,0 +1,35 @@
+from invoke import task
+from invoke.watchers import Responder
+
+# ORIGINAL
+#@task
+#def answers(c):
+#    responder = Responder(
+#        pattern=r" Separate source and build directories",
+#        response="n\n",
+#    )
+#    c.run("sphinx-quickstart docs", watchers=[responder])
+
+@task
+def answers(c):
+    responder1 = Responder(
+        pattern=r" Separate source and build directories",
+        response="n\n",
+    )
+    responder2 = Responder(
+        pattern=r" Project name",
+        response="Test\n",
+    )
+    responder3 = Responder(
+        pattern=r" Author name\(s\)",
+        response="Will\n",
+    )
+    responder4 = Responder(
+        pattern=r" Project release",
+        response="\n",
+    )
+    responder5 = Responder(
+        pattern=r" Project language",
+        response="\n",
+    )
+    c.run("sphinx-quickstart docs", watchers=[responder1, responder2, responder3, responder4, responder5])
