1cc7e2cf61631992971d9b491c387ae90efe65e8
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2022-07-05 13:45:12 -0500

    changed prompt answer

--- tasks.py
+++ tasks.py
@@ -14,7 +14,7 @@ from invoke.watchers import Responder
 @task
 def answers(c):
     responder1 = Responder(
-        pattern=r" Separate source and build directories (y/n)",
+        pattern=r" Separate source and build directories",
         response="\n",
     )
     responder2 = Responder(
